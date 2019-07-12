e81a333bd2bc731c10d35a2ba9ad6333acafadae
roy <578225840@qq.com>
2019-07-12 21:54:41 +0800

    上传ALlure项目 Signed-off-by: roy <578225840@qq.com>

---
+++ src/test/java/com/testing/testng/VisitBaidu.java
@@ -0,0 +1,19 @@
+package com.testing.testng;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+
+public class VisitBaidu {
+	//需要执行的测试方法
+  @Test
+  @Parameters({"webdriver","url"})
+  public void f(String webdriver,String url) {
+	  System.setProperty("webdriver.chrome.driver", webdriver);
+	  WebDriver driver=new ChromeDriver();
+	  driver.get(url);
+  }
+  
+}
